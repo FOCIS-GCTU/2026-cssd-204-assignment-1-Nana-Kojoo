0d36d23be3ea27270510501f64aca2b1e22ef5d7
Nana-Kojoo <asareelvis200@gmail.com>
2026-05-31 01:56:00 +0000

    Add initials.py assignment

---
+++ initials.py
@@ -0,0 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in basic print statements.
+# Assignment Number: 2
+#
+# Name: Asare Elvis Kojo
+# STUDENT ID: 2425402979
+# Email: 2425402979@line.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Asare Elvis Kojo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # This function prints the initials AEK using only basic print statements.
+    print()
+    print("...AEK")
+    print()
+
+    print("...A.........EEEEEEEEEEE...KK......KK****")
+    print("..A.A........E...........E..KK.....KK****")
+    print(".A...A.......E...........E..KK....KK****")
+    print("A.....A......EEEEEEEEEEE...KKKKKKKK****")
+    print("AAAAAAAAA....E...........E..KK....KK****")
+    print("A.......A....E...........E..KK.....KK****")
+    print("A.......A....EEEEEEEEEEE...KK......KK****")
+    print("A.......A....E...........E..KK.....KK****")
+    print("A.......A....E...........E..KK......KK****")
+    print("A.......A....EEEEEEEEEEE...KK.....KK****")
+
+    print()
+
+
+
+main()
